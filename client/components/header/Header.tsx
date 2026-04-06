9354f78cbf219379e3eeee38bb38d4294bc02ee0
Dmytro <doronindmytro623@gmail.com>
2026-04-06 16:22:03 +0200

    feat: logo

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { Logo } from '@/components/logo/Logo';
+
 export const Header = () => {
     return (
-        <header className="h-20 py-6 bg-white">
+        <header className="h-20 bg-white">
             <div className="h-full w-full flex items-center justify-between container-centered">
-                <div>logo</div>
+                <Logo />
                 <div>Have an account? Login</div>
             </div>
         </header>
---
+++ client/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button/Button';
+
+export const Logo = () => {
+    return (
+        <Button variant="link">
+            <div className="flex items-center justify-center text-[18px] bg-dark text-white w-8 h-8 rounded-sm">
+                ET
+            </div>
+        </Button>
+    );
+};
