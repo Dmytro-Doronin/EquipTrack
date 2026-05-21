eaad2ab49750d3b6feaca17f779cb483e5dabc0d
Dmytro <doronindmytro623@gmail.com>
2026-05-21 20:25:47 +0200

    fix: cookie 2

--- server/app/api/auth.py
+++ server/app/api/auth.py
@@ -49,7 +49,7 @@ def set_refresh_token_cookie(response: Response, refresh_token: str) -> None:
         value=refresh_token,
         httponly=True,
         secure=True,
-        samesite="none",
+        samesite="lax",
         max_age=60 * 60 * 24 * settings.refresh_token_expires_days,
         path="/",
     )
